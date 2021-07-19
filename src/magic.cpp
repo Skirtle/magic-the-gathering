91fbc6d2aca01eb509359a10df130422f48d4316
Dalton <daltonkajander@knights.ucf.edu>
2021-07-19 18:15:59 -0400

    Creature type created

--- src/magic.cpp
+++ src/magic.cpp
@@ -1,6 +1,7 @@
 #include "magic.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 std::string Card::getName()
 {
@@ -11,3 +12,11 @@ void Card::setName(std::string name)
 {
     this->name = name;
 }
+
+// Dalton's work
+int Creature::getBasePower() { return basePower; }
+int Creature::getBaseToughness() { return baseToughness; }
+std::vector<std::string> Creature::getSubtypes() { return subtypes; }
+void Creature::setBasePower(int power) { this->basePower = power; }
+void Creature::setBaseToughness(int toughness) { this->baseToughness = toughness; }
+void Creature::setSubtypes(std::vector<std::string> subtypes) {this->subtypes = subtypes; }
--- src/magic.h
+++ src/magic.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Card
 {
@@ -9,4 +10,19 @@ public:
     void setName(std::string name);
 protected:
     std::string name;
+};
+
+class Creature : public Card {
+private:
+    // attributes
+    int basePower, baseToughness;
+    std::vector<std::string> subtypes;
+public:
+    // methods
+    int getBasePower();
+    int getBaseToughness();
+    std::vector<std::string> getSubtypes();
+    void setBasePower(int power);
+    void setBaseToughness(int toughness);
+    void setSubtypes(std::vector<std::string>);
 };
